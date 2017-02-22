3356ea4e686a1d47ae5d2d4c3cbc1832ce2df626
Michael Smith <mikejsmitty@gmail.com>
2017-02-22 01:25:05 -0600

    Just kidding

--- edkey.go
+++ edkey.go
@@ -48,7 +48,8 @@ func MarshalED25519PrivateKey(key ed25519.PrivateKey) []byte {
 		//fmt.Fprintln(os.Stderr, "ed25519.PublicKey type assertion failed on an ed25519 public key. This should never ever happen.")
 		return nil
 	}
-	pk1.Pub = []byte(pk)
+	pubKey := []byte(pk)
+	pk1.Pub = pubKey
 
 	// Add our private key
 	pk1.Priv = []byte(key)
